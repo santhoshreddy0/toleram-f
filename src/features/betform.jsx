0c9319c37edf401027bef5edb9fdfe8a38bb908a
mayuri <mayuriwasu2000@gmail.com>
2023-05-15 17:27:18 +0530

    fixed ui

--- src/features/betform.jsx
+++ src/features/betform.jsx
@@ -37,7 +37,7 @@ export default function BetForm(props) {
 
     return (
         <>
-            <div className="mx-auto lg:w-1/2 min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="mx-auto lg:w-3/5 min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto">
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         {props?.title}
@@ -50,7 +50,7 @@ export default function BetForm(props) {
                     >
                         <div>
                             <div className="">
-                                <div className="sm:flex justify-between">
+                                <div className="sm:flex justify-around">
                                     <div>
                                         <label
                                             htmlFor="email"
@@ -97,7 +97,7 @@ export default function BetForm(props) {
                                     </div>
                                 </div>
 
-                                <div className="sm:flex justify-between">
+                                <div className="sm:flex justify-around">
                                     <div>
                                         <label
                                             htmlFor="email"
@@ -144,7 +144,7 @@ export default function BetForm(props) {
                                     </div>
                                 </div>
 
-                                <div className="sm:flex justify-between">
+                                <div className="sm:flex justify-around">
                                     <div>
                                         <label
                                             htmlFor="email"
@@ -186,7 +186,7 @@ export default function BetForm(props) {
                                 />
                                     </div>
                                 </div>
-                                <div className="sm:flex justify-between">
+                                <div className="sm:flex justify-around">
                                     <div>
                                         <label
                                             htmlFor="email"
@@ -229,7 +229,7 @@ export default function BetForm(props) {
                                     </div>
                                 </div>
 
-                                <div className="sm:flex justify-between">
+                                <div className="sm:flex justify-around">
                                     <div>
                                         <label
                                             htmlFor="email"
@@ -272,7 +272,7 @@ export default function BetForm(props) {
                                     </div>
                                 </div>
 
-                                <div className="sm:flex justify-between">
+                                <div className="sm:flex justify-around">
                                     <div>
                                         <label
                                             htmlFor="email"
@@ -319,7 +319,7 @@ export default function BetForm(props) {
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="flex w-full lg:w-4/5  mx-auto justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 bet now
                             </button>
