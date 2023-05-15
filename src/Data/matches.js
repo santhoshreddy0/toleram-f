7f7ba1f00bf68e31461b30e3956938f6ce792dfb
Santhosh Reddy <mail2myweb@gmail.com>
2023-05-15 13:23:53 +0530

    data for matches and teams added

---
+++ src/Data/matches.js
@@ -0,0 +1,30 @@
+export var matches = [
+    {   
+        "id" : 1,
+        "team1" : "team-A",
+        "team2" : "team-B",
+        "start_time" : "2023-05-15 15:00:00"
+    },
+    {
+        "id" : 2,
+        "team1" : "team-C",
+        "team2" : "team-D",
+        "start_time" : "2023-05-16 15:00:00"
+    }
+]
+
+
+// Questions:
+
+// who_wins,
+// who_wins_bet,
+// who_wins_toss,
+// who_wins_toss_bet,
+// most_runs_male,
+// most_runs_male_bet,
+// best_female_player,
+// best_female_player_bet,
+// first_inn_score,
+// first_inn_score_bet,
+// max_sixes,
+// max_sixes_bet
---
+++ src/Data/teams.js
@@ -0,0 +1,42 @@
+export var teams = {
+    "team-A" : [
+        {
+            "name":"player1",
+            "gender":"male"
+        },
+        {
+            "name":"player2",
+            "gender":"female"
+        }
+    ],
+    "team-B" : [
+        {
+            "name":"player3",
+            "gender":"male"
+        },
+        {
+            "name":"player4",
+            "gender":"female"
+        }
+    ],
+    "team-C" : [
+        {
+            "name":"player1",
+            "gender":"male"
+        },
+        {
+            "name":"player2",
+            "gender":"female"
+        }
+    ],
+    "team-D" : [
+        {
+            "name":"player3",
+            "gender":"male"
+        },
+        {
+            "name":"player4",
+            "gender":"female"
+        }
+    ]
+}
