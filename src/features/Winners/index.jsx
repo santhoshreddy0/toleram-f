c4fd03537cab02e9dce5640a7856ff7f84fb7781
Santhosh Reddy <mail2myweb@gmail.com>
2023-05-21 14:50:28 +0530

    new changes

--- src/features/Winners/index.jsx
+++ src/features/Winners/index.jsx
@@ -18,33 +18,33 @@ function index() {
     return <div> Unable load rounds data</div>;
   }
   const roundsData = rounds.rounds;
-
-  if (roundsData[3].status == "1") {
-    const existingData = winnersData.winners.find((w) => w?.round == '3');
+  if (roundsData[3]?.status == "1") {
+    const existingData = winnersData.winners.find((w) => w?.round == '4');
     return (
       <WinnerForm
         roundData={roundsData[3]}
         existingData={existingData ? existingData : false}
       />
     );
-  } else if (roundsData[2].status == "1") {
-    const existingData = winnersData.winners.find((w) => w?.round == '2');
+  } else if (roundsData[2]?.status == "1") {
+    const existingData = winnersData.winners.find((w) => w?.round == '3');
     return (
       <WinnerForm
         roundData={roundsData[2]}
         existingData={existingData ? existingData : false}
       />
     );
-  } else if (roundsData[1].status == "1") {
-    const existingData = winnersData.winners.find((w) => w?.round == '1');
+  } else if (roundsData[1]?.status == "1") {
+    console.log(winnersData.winners, '2');
+    const existingData = winnersData.winners.find((w) => w?.round == '2');
     return (
       <WinnerForm
         roundData={roundsData[1]}
         existingData={existingData ? existingData : false}
       />
     );
-  } else if(roundsData[0].status == "1"){
-    const existingData = winnersData.winners.find((w) => w?.round == '0');
+  } else if(roundsData[0]?.status == "1"){
+    const existingData = winnersData.winners.find((w) => w?.round == '1');
     return (
       <WinnerForm
         roundData={roundsData[0]}
