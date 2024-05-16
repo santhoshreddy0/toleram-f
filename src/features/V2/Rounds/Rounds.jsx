367b79108e79165c8b86070f5158a729d457dbe2
Santhosh Reddy <mail2myweb@gmail.com>
2024-05-16 20:50:24 +0530

    UI updated

--- src/features/V2/Rounds/Rounds.jsx
+++ src/features/V2/Rounds/Rounds.jsx
@@ -13,40 +13,40 @@ function Rounds() {
     }
     return (
         <MenuTabs>
-            <ul role="list" className="divide-y divide-gray-100 mx-5">
+            <ul role="list" className=" mx-5 grid grid-cols-1 md:grid-cols-2">
                 {rounds?.rounds?.map((round) => (
-                    <li key={round.id} className="">
+                    <li key={round.id} className="border md:max-w-lg m-5 rounded-lg border-white px-5 bg-gray-800">
                         <Link
                             to={`/rounds/${round.id}`}
                             className="flex justify-start gap-x-6 py-5"
                         >
-                            <div className="flex min-w-0 gap-x-4">
+                            <div className="flex min-w-0 gap-x-4 md:text-center md:mx-auto">
                                 {round.can_bet == "0" ? (
                                     <ShieldExclamationIcon className="h-10 w-10 text-red-500" />
                                 ) : (
                                     <FireIcon className="h-10 w-10 text-yellow-500" />
                                 )}
                                 <div className="min-w-0 flex-auto">
-                                    <p className="text-sm font-semibold leading-6 text-gray-900">
+                                    <p className="text-sm font-semibold leading-6">
                                         {round.round_name}
                                     </p>
-                                </div>
-                            </div>
-                            <div className=" shrink-0 sm:flex sm:flex-col sm:items-end">
-                                {round.can_bet == "0" ? (
-                                    <p className="mt-1 text-xs leading-5 text-gray-500">
-                                        Not Active{" "}
-                                    </p>
-                                ) : (
-                                    <div className="mt-1 flex items-center gap-x-1.5">
-                                        <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                                            <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                                        </div>
-                                        <p className="text-xs leading-5 text-gray-500">
-                                            Active
-                                        </p>
+                                    <div className=" shrink-0 sm:flex sm:flex-col sm:items-start text-left">
+                                        {round.can_bet == "0" ? (
+                                            <p className="mt-1 text-xs leading-5 text-gray-300">
+                                                Not Active{" "}
+                                            </p>
+                                        ) : (
+                                            <div className="mt-1 flex items-center gap-x-1.5">
+                                                <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                                                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                                                </div>
+                                                <p className="text-xs leading-5 text-gray-500">
+                                                    Active
+                                                </p>
+                                            </div>
+                                        )}
                                     </div>
-                                )}
+                                </div>
                             </div>
                         </Link>
                     </li>
