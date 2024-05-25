a4e781be4f06ff7867399620a42d0186e53260a1
Santhosh Reddy <mail2myweb@gmail.com>
2024-05-25 07:44:14 +0530

    logos updated

--- src/Data/teams.js
+++ src/Data/teams.js
@@ -9,60 +9,60 @@ export var teams = [
     id: "1",
     team_name: "POWER BRONCOS",
     team_logo:
-      "https://tolaram.retool.com/api/file/bc35fff2-b73e-43e8-942e-65141d857a7c",
+      "https://tolaram.retool.com/api/file/3a8523be-b47f-4b1e-a8da-646f2e5e5cb3",
   },
   {
     id: "2",
     team_name: "CORPORATE FALCONS",
     team_logo:
-      "https://tolaram.retool.com/api/file/08b6b449-dd3b-4f82-b37f-bb4a1718a7a4",
+      "https://tolaram.retool.com/api/file/fa43b813-2657-44df-a1f6-8217202bcd9f",
   },
   {
     id: "3",
     team_name: "KELLOGG'S CONQUERORS",
     team_logo:
-      "https://tolaram.retool.com/api/file/44bf17b0-e862-4e62-a094-32d670edda6e",
+      "https://tolaram.retool.com/api/file/94a70eb2-29b8-4094-a201-48c7444366d7",
   },
   {
     id: "4",
     team_name: "NUTRIFY LUCKY STARS",
     team_logo:
-      "https://tolaram.retool.com/api/file/9a3111f7-1879-4974-9e68-0aa626d7cf4d",
+      "https://tolaram.retool.com/api/file/af225c4b-f6de-4c93-8590-27ccb7559da8",
   },
   {
     id: "5",
     team_name: "INDOMIE WARRIORS",
     team_logo:
-      "https://tolaram.retool.com/api/file/2175b402-524e-4423-adb6-829b413303ec",
+      "https://tolaram.retool.com/api/file/22651032-ad6f-4461-9841-f9addea40532",
   },
   {
     id: "6",
     team_name: "BHN HUNTERS",
     team_logo:
-      "https://tolaram.retool.com/api/file/88e86ec2-35fa-466e-8ba1-4166629c904b",
+      "https://tolaram.retool.com/api/file/9a79b7b3-477b-4f10-8b93-331c49edfd81",
   },
   {
     id: "7",
     team_name: "COLGATE WHITE WARRIORS",
     team_logo:
-      "https://tolaram.retool.com/api/file/7e384f86-6ce3-40d7-8a80-726f39da73c6",
+      "https://tolaram.retool.com/api/file/3f883afb-ce46-4d21-a845-eef4206b5b15",
   },
   {
     id: "8",
     team_name: "DANOLFZ LANCERS",
     team_logo:
-      "https://tolaram.retool.com/api/file/16b4faa3-3d4a-4686-91fc-07e2a776b202",
+      "https://tolaram.retool.com/api/file/a13c2764-7f53-48d1-a929-5cacc75e33ea",
   },
   {
     id: "9",
     team_name: "MINIMIE VIKINGS",
     team_logo:
-      "https://tolaram.retool.com/api/file/195c9f8a-1e1c-4bd1-afb3-e76f275083c8",
+      "https://tolaram.retool.com/api/file/984895b9-32d7-4921-a220-dfd92bebe499",
   },
   {
     id: "10",
     team_name: "DUFIL CRUSADERS",
     team_logo:
-      "https://tolaram.retool.com/api/file/513feef4-5556-41e7-83f8-8cef4b315265",
+      "https://tolaram.retool.com/api/file/903f7bd4-2f3a-4e3f-9773-b4919d2995d1",
   },
 ];
