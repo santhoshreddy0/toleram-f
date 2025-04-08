3d0d9d9110bcf0642cbf5a667acb070c65bff57f
mintureddy25 <chappetasaitejareddy@gmail.com>
2025-04-08 23:06:49 +0530

    bug fix

--- src/features/Layout/AdminLayout.jsx
+++ src/features/Layout/AdminLayout.jsx
@@ -63,6 +63,7 @@ export default function AdminLayout() {
     navigate("/matches");
   }
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  
 
   const navigation = nav.map((item) => {
     return {
@@ -74,6 +75,18 @@ export default function AdminLayout() {
   return (
     <>
       <div>
+      <div className="lg:hidden flex justify-end">
+        <button
+          type="button"
+          className="text-gray-100 hover:text-gray-600 focus:outline-none"
+          onClick={() => setSidebarOpen(true)}
+        >
+          <span className="sr-only">Open sidebar</span>
+          <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
         <Dialog
           open={sidebarOpen}
           onClose={setSidebarOpen}
@@ -104,12 +117,12 @@ export default function AdminLayout() {
                   </button>
                 </div>
               </TransitionChild>
-              {/* Sidebar component, swap this element with another sidebar if you like */}
+              
               <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
                 <div className="flex h-16 shrink-0 items-center">
                   <img
-                    alt="Your Company"
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Tolaram"
+                    src="./toleram-icon.png"
                     className="h-8 w-auto"
                   />
                 </div>
@@ -123,9 +136,9 @@ export default function AdminLayout() {
                               to={item.href}
                               className={classNames(
                                 item.current
-                                  ? "bg-gray-50 text-indigo-600"
-                                  : "text-gray-700 hover:bg-gray-800 hover:text-white",
-                                "group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold"
+                                ? "bg-gray-800 text-gray-100 border border-gray-700"
+                                : "text-gray-400 hover:bg-gray-700 hover:text-white",
+                              "group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold"
                               )}
                             >
                               <item.icon
@@ -150,9 +163,9 @@ export default function AdminLayout() {
           </div>
         </Dialog>
         <div className="max-w-7xl mx-auto min-h-full md:mt-16">
-          {/* Static sidebar for desktop */}
+         
           <div className="hidden lg:fixed lg:inset-y-3 lg:z-50 lg:flex lg:w-72 lg:flex-col mt-[6.5rem] border-r border-gray-700">
-            {/* Sidebar component, swap this element with another sidebar if you like */}
+            
             <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 mt-12">
               <nav className="flex flex-1 flex-col">
                 <ul role="list" className="flex flex-1 flex-col gap-y-7">
