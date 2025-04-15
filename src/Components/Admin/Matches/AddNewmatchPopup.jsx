0f706ece549723b4b33b29de172a797140c80d64
mayuriwasu1 <mayuriwasu2000@gmail.com>
2025-04-15 19:26:30 +0530

    fixed calender color

--- src/Components/Admin/Matches/AddNewmatchPopup.jsx
+++ src/Components/Admin/Matches/AddNewmatchPopup.jsx
@@ -130,7 +130,7 @@ export default function CreateNewMatch({ open, setOpen, matchId }) {
                     onChange={(e) =>
                       setMatchData({ ...matchData, matchTime: e.target.value })
                     }
-                    className="w-full bg-gray-900 rounded-md border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    className="datetime-picker w-full bg-gray-900 rounded-md border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
                   />
 
                   <select
