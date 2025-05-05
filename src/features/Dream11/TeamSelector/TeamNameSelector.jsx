7f02d028fb1a48941c4850f03b72473485ca22cd
mintureddy25 <chappetasaitejareddy@gmail.com>
2025-05-05 15:38:08 +0530

    added teamnam in super12 and rank in admin user dahsboard

--- src/features/Dream11/TeamSelector/TeamNameSelector.jsx
+++ src/features/Dream11/TeamSelector/TeamNameSelector.jsx
@@ -24,33 +24,10 @@ export default function TeamNameInput({ teamName, onChange }) {
             d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
           />
         </svg>
-        <h3 className="text-lg font-semibold text-white">Team Registration</h3>
+        <h3 className="text-lg font-semibold text-white">Super12 Team Name</h3>
       </div>
 
       <div className="mb-6">
-        <div className="flex items-center mb-2">
-          <svg
-            className="h-4 w-4 text-indigo-400 mr-2"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-            />
-          </svg>
-          <label
-            htmlFor="teamName"
-            className="block text-sm font-medium text-gray-200"
-          >
-            Super1 Team Name
-          </label>
-        </div>
-
         <div className="relative">
           <input
             id="teamName"
